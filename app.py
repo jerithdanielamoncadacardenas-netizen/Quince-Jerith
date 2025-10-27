d7efad4545bb662cd439b62ec6196d914dbeb2fa
jerithdanielamoncadacardenas-netizen <jerithdanielamoncadacardenas@gmail.com>
2025-10-26 21:22:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,3 @@ def delete_file(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
